f9df835d187fbcbe9d5efee0156803b4eb6ac72b
Edoardo <edo.allari@gmail.com>
2022-08-27 11:05:25 +0200

    Day 1 project

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome in the best band name generator!")
+city = input("In which city are you born?\n")
+pet = input("Which is your favourite pet?\n")
+print("I think " + city + " " + pet + " is an awesome band name!")
