e091069e077f06f72f95d9ae1fb5653bde0b7e92
Edoardo <edo.allari@gmail.com>
2022-08-28 11:23:05 +0200

    Day 2 project - tip calculator

---
+++ Day 2/tip_calculator.py
@@ -0,0 +1,12 @@
+print("Welcome to the tip calculator.")
+
+totalBill = float(input("What was the total bill? "))
+percentageTip = float(input("What percentage tip would you lke to give? "))
+eaters = int(input("How many were you?"))
+
+totalCostOfMeal = totalBill * round(percentageTip / 100 + 1, 2)
+
+# Forcing price per person to 2 float digits
+priceForPerson = "{:.2f}".format(round(totalCostOfMeal / eaters, 2))
+
+print(f"Each person should pay {priceForPerson} €")
