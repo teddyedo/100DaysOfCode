d10a35776c6211025176a68b2382f10f55034faf
Edoardo <edo.allari@gmail.com>
2022-09-01 08:45:27 +0200

    Day 6 project

---
+++ Day 6/reeborg.py
@@ -0,0 +1,21 @@
+# This code has to be executed on Reeborg's World to make the little robot escape from the maze
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#     move()
+
+
+# while front_is_clear():
+#     move()
+
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
