d01ab9c4b304fcccf7f509ccda75d91ca36311f4
teddyedo <edo.allari@gmail.com>
2022-09-10 23:03:17 +0200

    Started Day 15 project

---
+++ Day 15/main.py
@@ -0,0 +1,103 @@
+from data import MENU
+from data import resources
+
+
+def chooseDrink():
+    return input("What would you like? (espresso/latte/cappuccino): ")
+
+
+def turnOff():
+    print("Coffee machine is in manteinance mode.")
+
+
+def showReport():
+    print("Coffee machine resources:")
+    print(f"Water: {resources['water']} ml")
+    print(f"Coffee: {resources['coffee']} g")
+    print(f"Milk: {resources['milk']} ml")
+    print(f"Money: ${resources['money']}")
+
+
+def resourcesAreEnough(drink):
+    drinkRecipe = MENU[drink]
+    if drink == "espresso":
+        if drinkRecipe["ingredients"]["water"] > resources["water"]:
+            print("Sorry, there isn't enough water")
+            return False
+        if drinkRecipe["ingredients"]["coffee"] > resources["coffee"]:
+            print("Sorry, there isn't enough coffee")
+            return False
+        return True
+    elif drink == "latte":
+        if drinkRecipe["ingredients"]["water"] > resources["water"]:
+            print("Sorry, there isn't enough water")
+            return False
+        if drinkRecipe["ingredients"]["coffee"] > resources["coffee"]:
+            print("Sorry, there isn't enough coffee")
+            return False
+        if drinkRecipe["ingredients"]["milk"] > resources["milk"]:
+            print("Sorry, there isn't enough milk")
+            return False
+        return True
+    elif drink == "cappuccino":
+        if drinkRecipe["ingredients"]["water"] > resources["water"]:
+            print("Sorry, there isn't enough water")
+            return False
+        if drinkRecipe["ingredients"]["coffee"] > resources["coffee"]:
+            print("Sorry, there isn't enough coffee")
+            return False
+        if drinkRecipe["ingredients"]["milk"] > resources["milk"]:
+            print("Sorry, there isn't enough milk")
+            return False
+        return True
+    else:
+        print("This drink is not supported")
+        return False
+
+
+def getTheCoins(drink):
+    print("Time to insert some money: ")
+    pennies = int(input("Insert pennies: "))
+    nickles = int(input("Insert nickles: "))
+    dimes = int(input("Insert dimes: "))
+    quarters = int(input("Insert quarters: "))
+
+    return round((pennies + nickles * 5 + dimes * 10 + quarters * 25) / 100, 2)
+
+
+def elaborateTransaction(drink, moneyAmount):
+    drinkCost = MENU[drink]["cost"]
+    if moneyAmount < drinkCost:
+        print(
+            f"Sorry, not enough money inserted for a {drink}. Money refunded.")
+        return False
+    else:
+        if moneyAmount > drinkCost:
+            print(f"I give you back $ {moneyAmount - drinkCost}")
+        resources["money"] += drinkCost
+        return True
+
+
+def prepareTheDrink(drink):
+    resources["water"] -= MENU[drink]["ingredients"]["water"]
+    resources["coffee"] -= MENU[drink]["ingredients"]["coffee"]
+    if drink == "latte" or drink == "cappuccino":
+        resources["milk"] -= MENU[drink]["ingredients"]["milk"]
+    print(f"Drink purchased. Enjoy your {drink}.")
+
+
+keepWorking = True
+resources["money"] = 0
+
+while keepWorking:
+    choice = chooseDrink()
+    if choice == "report":
+        showReport()
+    elif choice == "off":
+        turnOff()
+        keepWorking = False
+    else:
+        if resourcesAreEnough(choice):
+            coinsInserted = getTheCoins(choice)
+            if elaborateTransaction(choice, coinsInserted):
+                prepareTheDrink(choice)
