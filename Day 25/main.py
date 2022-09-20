fc32858e77e67e4f8b9a9cb200cefbc95072d37b
Edoardo Allari <edoardo.allari@addvalue.it>
2022-09-20 19:13:17 +0200

    Init Day 25 project

---
+++ Day 25/main.py
@@ -0,0 +1,9 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("US State game")
+screen.addshape("Day 25/blank_states_img.gif")
+
+turtle.shape("Day 25/blank_states_img.gif")
+
+screen.exitonclick()
