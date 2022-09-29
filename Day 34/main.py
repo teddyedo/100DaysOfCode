f658c1e0d31337f96c53f070029002d9d74174fa
Edoardo Allari <edoardo.allari@addvalue.it>
2022-09-29 08:35:31 +0200

    Started Day 34 project

---
+++ Day 34/main.py
@@ -0,0 +1,33 @@
+from this import d
+from question_model import Question
+from quiz_brain import QuizBrain
+import requests
+
+question_bank = []
+
+
+def get_new_questions():
+    parameters = {
+        "amount": 10,
+        "type": "boolean"
+    }
+    response = requests.get(
+        url="https://opentdb.com/api.php", params=parameters)
+    response.raise_for_status()
+    return response.json()["results"]
+
+
+for question in get_new_questions():
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
